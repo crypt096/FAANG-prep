1bbb8955cbe789618dec762d8be1a924124f6e46
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2021-12-16 17:09:41 +0100

    Update brute-force-solution.js

--- strings/LongestSubstringWithoutRepeatingCharacters/brute-force-solution.js
+++ strings/LongestSubstringWithoutRepeatingCharacters/brute-force-solution.js
@@ -3,5 +3,19 @@ const lengthOfLongestSubstring = s => {
     return s.length;
   }
 
-  
+  let longest = 0;
+
+  for(let left = 0; left < s.length; left++) {
+    let seenChars = {}, currentLength = 0;
+    for(let right = left; right < s.length; right++) {
+      const currentChar = s[right];
+      if(!seenChars[currentChar]) {
+        currentLength++;
+        seenChars[currentChar] = true;
+        longest = Math.max(longest, currentLength);
+      } else {
+        break;
+      }
+    }
+  }
 }
